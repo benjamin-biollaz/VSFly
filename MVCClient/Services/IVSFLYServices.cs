079a470cb978a87a1aacb9cc3b19d3a4ee0d5e8e
benjamin <benjamin.biollaz@students.hevs.ch>
2022-06-02 16:57:15 +0200

    interface pushed

--- MVCClient/Services/IVSFLYServices.cs
+++ MVCClient/Services/IVSFLYServices.cs
@@ -10,5 +10,11 @@ namespace MVCClient.Services
     public interface IVSFLYServices
     {
        public Task<IEnumerable<FlightM>> GetFlights();
+       public Task<float> GetFlightSalePrice();
+       public Task<float> GetTotalSalePrice();
+       public Task<int> BookFlight(int id, string firstName, string lastName);
+       public Task<FlightM> GetFlight(int id);
+       public Task<float> GetAveragePriceByDestination(string destination);
+
     }
 }
